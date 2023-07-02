9ab80e7e596cacfba0f4191494c195e470b1a665
Junior Fernando <junior_618@hotmail.com>
2023-07-02 18:30:09 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalago01/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo01.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo01.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+    }
+}
